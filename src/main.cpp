a55050183c0924c773406eed301dc5cb646967d6
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-09-06 20:16:49 +0500

    файлы

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,19 @@
 
 int main(){
   std::cout << "Fishing" << std::endl;
+  std::string fish;
+  std::ofstream file_river("..\\src\\File\\river.txt");
+  std::ofstream file_basket("..\\src\\File\\basket.txt", std::ios::app);
 
+  if(file_river && file_basket){
+    std::cout << "Enter name fish." << std::endl;
+    std::cin >> fish;
+    file_basket << fish << "\n";
+  }else{
+    std::cout << "Couldn't open the file!" << std::endl;
+  }
+  file_river.close();
+  file_basket.close();
   return 0;
 }
 
